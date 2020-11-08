116b213b32b94177ed7555c7d91339da26670f99
Mostafa Tavassoli Norouzi <68356134+mosTavassoli@users.noreply.github.com>
2020-11-08 22:01:34 +0100

    feat: add LoginPage [PULSEBS-45]

---
+++ client/src/data/fakeUsers.js
@@ -0,0 +1,16 @@
+export const USERS = [
+  {
+    id: 1,
+    name: "Teacher1",
+    email: "teacher@polito.it",
+    pass: "123",
+    role: "teacher",
+  },
+  {
+    id: 2,
+    name: "Student1",
+    email: "student@polito.it",
+    pass: "123456",
+    role: "student",
+  },
+];
