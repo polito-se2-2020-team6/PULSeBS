6e38acbac81cb7c511d40d14fb217acce4d36531
Hesam Mashhadi Mohammad <s271199@studenti.polito.it>
2020-11-09 20:14:39 +0100

    feat: improving nav bar [PULSEBS-35]

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,4 +1,6 @@
 import React, { Component, Fragment } from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from 'react-bootstrap/Nav';
 import { Link } from "react-router-dom";
 
 class Navigation extends Component {
@@ -6,34 +8,16 @@ class Navigation extends Component {
   render() {
     return (
       <Fragment>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-              <Link to="/">
-                <li className="navbar-brand">
-                  PULSE <span className="sr-only">(current)</span>
-                </li>
-              </Link>
-              <Link to="/Student">
-                <li className="nav-item nav-link">Student</li>
-              </Link>
-              <Link to="/Teacher">
-                <li className="nav-item nav-link">Teacher</li>
-              </Link>
-            </ul>
-          </div>
-        </nav>
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+          <Navbar.Brand href="/">PULSEBS</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="/student">Student</Nav.Link>
+              <Nav.Link href="/teacher">Teacher</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
       </Fragment>
     );
   }
--- client/src/components/Student.js
+++ client/src/components/Student.js
@@ -3,7 +3,10 @@ import React from 'react';
 const Student = () => {
     return ( 
         <>
-            <h1>Student</h1>
+            <h1>Student Page</h1>
+            <button type="button" class="btn btn-primary">Book A Seat</button>
+            <button type="button" class="btn btn-success">Upcoming Lectures</button>
+
         </>
      );
 }
