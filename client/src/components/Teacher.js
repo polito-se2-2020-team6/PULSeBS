9ae5b22236cade913a5942d3155d8d07fa709e6c
MassimoMissio <74026335+MassimoMissio@users.noreply.github.com>
2020-11-09 23:29:43 +0100

    Show list of fake students in teacher's page [PULSEBS-37]

--- client/src/components/Teacher.js
+++ client/src/components/Teacher.js
@@ -3,18 +3,18 @@ import { Col, Container, Row, Button, ListGroup } from "react-bootstrap";
 
 
 var listaCorsi = [{
-    corso: "storia",
-    studenti: new Array("ettore", "carlo")
+    corso: "History",
+    studenti: new Array("ettore", "carlo","luca", "camarcorlo")
 }, {
-    corso: "geometria",
+    corso: "Geometry",
     studenti: new Array("ettore", "carlo")
     
 }, {
-    corso: "storia",
-    studenti: new Array("ettore", "carlo")
+    corso: "Software Engineering 2",
+    studenti: new Array("")
 }, {
-    corso: "storia",
-    studenti: new Array("ettore", "carlo")
+    corso: "Analisi",
+    studenti: new Array("ludo", "carlo","max")
 }]
 
 class Teacher extends React.Component {
@@ -22,6 +22,8 @@ class Teacher extends React.Component {
       super(props);
   
       this.state = {
+          course : listaCorsi[0].corso,
+          students : listaCorsi[0].studenti
       };
     }
 
@@ -29,39 +31,54 @@ class Teacher extends React.Component {
 
     render() {
         return (<>
-            <Container fluid className="mt-3 rounded-circle"> 
+            <Container fluid className="mt-3 "> 
                 <Row>
-                    <Col >
-                        <ListGroup as="ul">
-                        <ListGroup.Item as="li" active>
-                        Studenti
-                        </ListGroup.Item>
-                        <ListGroup.Item as="li">Carlo</ListGroup.Item>
-                        <ListGroup.Item as="li">
-                        Gianni
-                        </ListGroup.Item>
-                        <ListGroup.Item as="li">Ettore</ListGroup.Item>
-                        </ListGroup>
-                    </Col>
                     <Col>
                         <ListGroup as="ul">
-                        <ListGroup.Item as="li" active>
-                        Lezioni
-                        </ListGroup.Item>
-                        {listaCorsi?.map((c) => (
-                          <ListGroup.Item as="li">{c.corso} <button type="button" class="close" aria-label="Close">
-                                                            <span aria-hidden="true">&times;</span>
-                                                        </button>
-                          </ListGroup.Item>
-                        ))}
+                            <ListGroup.Item as="li" active>
+                                Lectures
+                            </ListGroup.Item>
+                                {listaCorsi?.map((c) => (
+                                    <button className="square" onClick={()=> this.setState({students: c.studenti, course: c.corso})}>
+                                        <ListGroup.Item as="li">{c.corso} 
+                                            <button type="button" class="close" aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>
+                                        </ListGroup.Item>
+                                    </button>
+                                ))}
                         </ListGroup>
                     </Col>
-                    <Col></Col>
+
+                    <Col >
+                        <ListGroup as="ul">
+                            <ListGroup.Item as="li" active>
+                                {this.state.course}'s students
+                            </ListGroup.Item>
+                            
+                                {this.state.students.map((s)=> (
+                                    <ListGroup.Item as="li">{s} 
+                                        
+                                    </ListGroup.Item>
+                                )
+                                )}
+                              
+                        </ListGroup>
+                    </Col>
+                    
+                    <Col>
+
+                    </Col>
                     <Col></Col>
                 </Row>
             </Container>
         </>);
     }
+}
+
+
+function funct_Student(corso){
+    this.setState({course : corso.corso , students : corso.studenti });
 }
 
 export default Teacher;
