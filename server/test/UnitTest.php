9998dd8b57b9a6e9ca1fc9a71084e3b34af24fe7
Alessandro Borione <s272070@studenti.polito.it>
2020-11-13 11:33:04 +0100

    Change dependency following deprecation
    gruzzle/gruzzle -> gruzzlehttp/gruzzle

--- server/test/UnitTest.php
+++ server/test/UnitTest.php
@@ -2,27 +2,20 @@
 
 use PHPUnit\Framework\TestCase;
 
-// namespace KnpU\CodeBattle\Tests;
+use GuzzleHttp\Client;
 
-use Guzzle\Http\Client;
-
-class ProgrammerControllerTest extends TestCase {
+class UnitTest extends TestCase {
 
 	public function testPOST() {
 		// create our http client (Guzzle)
-		$client = new Client('http://127.0.0.1:8080', array(
-			'request.options' => array(
-				'exceptions' => false,
-			)
-		));
+		$client = new Client(array('baseUri' => 'http://127.0.0.1:8080'));
 
 		$data = array(
 			'username' => 'francescop',
 			'password' => '12345689',
 		);
 
-		$request = $client->post('/API/REST.php/api/login', null, json_encode($data));
-		$response = $request->send();
+		$response = $client->request('POST', 'http://127.0.0.1:8080/API/REST.php/api/login', $data);
 
 		$this->assertEquals(200, $response->getStatusCode());
 		$data = json_decode($response->getBody(true), true);
