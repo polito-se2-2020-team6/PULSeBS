74533646a09e7a7408f13905eef44d5d510cb2b5
MassimoMissio <74026335+MassimoMissio@users.noreply.github.com>
2020-11-13 21:22:34 +0100

    Modify Teacher page, course and lecture shown in the right way

--- client/src/components/Teacher.js
+++ client/src/components/Teacher.js
@@ -1,19 +1,36 @@
 import React from "react";
 import { Col, Container, Row, Tabs, Tab, ListGroup } from "react-bootstrap";
 
+/*  listaLezione -> getLectures 
+    corso -> courseId
+    lezione -> lectureId
+    studenti -> vector of Students
+*/
 
-var listaCorsi = [{
-    corso: "13:30",
+var listaLezioni = [{
+    corso: "History",
+    lezione: "HY-01",
     studenti: new Array("ettore", "carlo","luca", "camarcorlo")
 }, {
-    corso: "14:30",
+    corso: "Geometry",
+    lezione: "GY-01",
     studenti: new Array("ettore", "carlo")
     
 }, {
-    corso: "15:30",
+    corso: "History",
+    lezione: "HY-02",
+    studenti: new Array("ettore", "camarcorlo")
+}, {
+    corso: "Software Engineering 2",
+    lezione: "SE2-01",
     studenti: new Array("")
 }, {
-    corso: "16:30",
+    corso: "Software Engineering 2",
+    lezione: "SE2-02",
+    studenti: new Array("Marco","Luca")
+}, {
+    corso: "Analisi",
+    lezione: "AI-01",
     studenti: new Array("ludo", "carlo","max")
 }]
 
@@ -24,9 +41,10 @@ class Teacher extends React.Component {
       super(props);
   
       this.state = {
-          totalCourses : listaCorsi,
-          course : listaCorsi[0].corso,
-          students : listaCorsi[0].studenti
+          totalLectures : listaLezioni,
+          course : listaLezioni[0].corso,
+          students : listaLezioni[0].studenti,
+          lecture : listaLezioni[0].lezione,
       };
     }
 
@@ -36,12 +54,14 @@ class Teacher extends React.Component {
     }
 
     deleteLecture(lecture){
-        this.setState({totalCourses : this.state.totalCourses.filter(c => c.corso !== lecture)})
+        this.setState({totalLectures : this.state.totalLectures.filter(c => c.corso !== lecture)})
         //collega all'API
 
     };
-
+    
     render() {
+        
+        const corsi =this.state.totalLectures.map((item) => item.corso ).filter((v,i,s)=> s.indexOf(v) === i ) ;
         return (<>
             <Container fluid className="mt-5 "> 
             
@@ -50,118 +70,49 @@ class Teacher extends React.Component {
                     
                 </Row>
                 
-                <Tabs defaultActiveKey="history"  id="noanim-tab-example">
-                <Tab eventKey="history" title="History">
-                <Row className="mt-5 justify-content-md-center">
-                    <Col md={4}>
-                        
-                    <Tab.Container id="list-group-tabs-example" >
-                        <ListGroup className="mt-5">
-                            {this.state.totalCourses?.map((c) => (
-                                    
-                                        <ListGroup.Item action active={c.corso===this.state.course} onClick={()=> this.setState({students: c.studenti, course: c.corso})}>{c.corso} 
-                                           
-                                            <button type="button" class="close" aria-label="Close" onClick={this.deleteLecture.bind(this, c.corso)}>
-                                                <span aria-hidden="true">&times;</span>
-                                            </button>
-                                        </ListGroup.Item>
-                                    
-                                ))}
-                        </ListGroup>
-                    </Tab.Container>
-
-                    </Col>
-                    <Col md={1}></Col>
-                    {/*<Col md={4}>
-                        <ListGroup as="ul">
-                            <ListGroup.Item as="li" active>
-                                <h1>Lectures</h1>
-                            </ListGroup.Item>
-                                {this.state.totalCourses?.map((c) => (
-                                    
-                                        <ListGroup.Item as="li" action  onClick={()=> this.setState({students: c.studenti, course: c.corso})}>{c.corso} 
-                                            <button type="button" class="close" aria-label="Close" onClick={this.deleteLecture.bind(this, c.corso)}>
-                                                <span aria-hidden="true">&times;</span>
-                                            </button>
-                                        </ListGroup.Item>
-                                    
-                                ))}
-                        </ListGroup>
-                    </Col> */}
-
-                    <Col md={4}>
-                        <ListGroup as="ul">
-                            <ListGroup.Item as="li" active>
-                                <h1>students booked</h1>
-                            </ListGroup.Item>
-                            
-                                {this.state.students.map((s)=> (
-                                    <ListGroup.Item as="li">{s} 
-                                        
+                <Tabs defaultActiveKey={this.state.totalLectures[0].corso}  id="noanim-tab-example">
+                    {corsi?.map((C_Id)=> (
+                         <Tab eventKey={C_Id} title={C_Id} >
+                             <Row className="mt-5 justify-content-md-center">
+                             <Col md={4} >
+                                <Tab.Container id="list-group-tabs-example" >
+                                    <ListGroup className="mt-5" >
+                                        {
+                                            this.state.totalLectures.filter(l => l.corso===C_Id)?.map((c) => (
+                                                        <ListGroup.Item action active={c.lezione===this.state.lecture} 
+                                                            onClick={()=> this.setState({students: c.studenti, course: c.corso, lecture: c.lezione})}>
+                                                            {c.lezione} 
+                                                            
+                                                            <button type="button" class="close" aria-label="Close" onClick={this.deleteLecture.bind(this, c.corso)}>
+                                                                <span aria-hidden="true">&times;</span>
+                                                            </button>
+                                                        </ListGroup.Item>
+                                            ))}
+                                    </ListGroup>
+                                </Tab.Container>
+                            </Col>
+                            <Col md={1}></Col>
+                            <Col md={4}>
+                                <ListGroup as="ul">
+                                    <ListGroup.Item as="li" active>
+                                        <h3>students booked</h3>
                                     </ListGroup.Item>
-                                )
-                                )}
-                              
-                        </ListGroup>
-                    </Col>  
-                </Row>
-                </Tab>
-                <Tab eventKey="Software Engineering 2" title="Software Engineering 2">
-                <Row className="mt-5 justify-content-md-center">
-                    <Col md={4}>
-                        
-                    <Tab.Container id="list-group-tabs-example" >
-                        <ListGroup className="mt-5">
-                            {this.state.totalCourses?.map((c) => (
                                     
-                                        <ListGroup.Item action active={c.corso===this.state.course} onClick={()=> this.setState({students: c.studenti, course: c.corso})}>{c.corso} 
-                                            <button type="button" class="close" aria-label="Close" onClick={this.deleteLecture.bind(this, c.corso)}>
-                                                <span aria-hidden="true">&times;</span>
-                                            </button>
-                                        </ListGroup.Item>
+                                        {this.state.students.map((s)=> (
+                                            <ListGroup.Item as="li">{s} 
+                                                
+                                            </ListGroup.Item>
+                                        )
+                                        )}
                                     
-                                ))}
-                        </ListGroup>
-                    </Tab.Container>
-
-                    </Col>
-                    <Col md={1}></Col>
-                    {/*<Col md={4}>
-                        <ListGroup as="ul">
-                            <ListGroup.Item as="li" active>
-                                <h1>Lectures</h1>
-                            </ListGroup.Item>
-                                {this.state.totalCourses?.map((c) => (
-                                    
-                                        <ListGroup.Item as="li" action  onClick={()=> this.setState({students: c.studenti, course: c.corso})}>{c.corso} 
-                                            <button type="button" class="close" aria-label="Close" onClick={this.deleteLecture.bind(this, c.corso)}>
-                                                <span aria-hidden="true">&times;</span>
-                                            </button>
-                                        </ListGroup.Item>
-                                    
-                                ))}
-                        </ListGroup>
-                    </Col> */}
-
-                    <Col md={4}>
-                        <ListGroup as="ul">
-                            <ListGroup.Item as="li" active>
-                                <h1>students booked</h1>
-                            </ListGroup.Item>
-                            
-                                {this.state.students.map((s)=> (
-                                    <ListGroup.Item as="li">{s} 
-                                        
-                                    </ListGroup.Item>
-                                )
-                                )}
-                              
-                        </ListGroup>
-                    </Col>  
-                </Row>
-                </Tab>
+                                </ListGroup>
+                            </Col> 
+                             </Row>
+                         </Tab>
+                    )
+                    
+                    )}
                 </Tabs>
-                
             </Container>
         </>);
     }
