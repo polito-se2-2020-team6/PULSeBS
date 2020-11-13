c6396a4578ab047d37c73f939f49b59fe8365026
Alessandro Borione <s272070@studenti.polito.it>
2020-11-13 12:57:04 +0100

    Fix login test

--- server/test/UnitTest.php
+++ server/test/UnitTest.php
@@ -6,19 +6,24 @@ use GuzzleHttp\Client;
 
 class UnitTest extends TestCase {
 
-	public function testPOST() {
+	public function testLogin() {
 		// create our http client (Guzzle)
-		$client = new Client(array('baseUri' => 'http://127.0.0.1:8080'));
+		$client = new Client();
 
 		$data = array(
 			'username' => 'francescop',
-			'password' => '12345689',
+			'password' => '123456789',
 		);
 
-		$response = $client->request('POST', 'http://127.0.0.1:8080/API/REST.php/api/login', $data);
+		$response = $client->request('POST', 'http://127.0.0.1:8080/API/REST.php/api/login', array('form_params' => $data));
 
 		$this->assertEquals(200, $response->getStatusCode());
 		$data = json_decode($response->getBody(true), true);
 		$this->assertArrayHasKey('success', $data);
+		$this->assertTrue($data['success']);
+		$this->assertArrayHasKey('userId', $data);
+		$this->assertEquals(1, $data['userId']);
+		$this->assertArrayHasKey('type', $data);
+		$this->assertEquals(0, $data['type']);
 	}
 }
