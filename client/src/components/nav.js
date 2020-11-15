218ea83625906f5b78e9537d4229edd70c17e176
Mostafa Tavassoli Norouzi <68356134+mosTavassoli@users.noreply.github.com>
2020-11-15 01:43:09 +0100

    fix: fix some bugs in nav components

--- client/src/components/nav.js
+++ client/src/components/nav.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
-import Navbar from "react-bootstrap/Navbar";
-import Nav from "react-bootstrap/Nav";
-// import { Link, NavLink } from "react-router-dom";
+// import Navbar from "react-bootstrap/Navbar";
+// import Nav from "react-bootstrap/Nav";
+import { NavLink } from "react-router-dom";
+import { Nav, Navbar, NavItem } from "react-bootstrap";
 
 import { AuthContext } from "../auth/AuthContext";
 import { ROLES } from "../data/consts";
@@ -21,24 +22,35 @@ class Navigation extends Component {
                   <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="container-fluid">
                       {context.authUser.type === ROLES.STUDENT && (
-                        <Nav.Item>
-                          <Nav.Link href="/student">Student</Nav.Link>
-                        </Nav.Item>
+                        <NavItem>
+                          <NavLink className="nav-link" to="/student/home">
+                            Student
+                          </NavLink>
+                        </NavItem>
+                      )}
+                      {context.authUser.type === ROLES.STUDENT && (
+                        <NavItem>
+                          <NavLink className="nav-link" to="/student/calender">
+                            Calender
+                          </NavLink>
+                        </NavItem>
                       )}
                       {context.authUser.type === ROLES.TEACHER && (
-                        <Nav.Item>
-                          <Nav.Link href="/teacher">Teacher</Nav.Link>
-                        </Nav.Item>
+                        <NavItem>
+                          <NavLink className="nav-link" to="/teacher/home">
+                            Teacher
+                          </NavLink>
+                        </NavItem>
                       )}
-                      <Nav.Item className="ml-auto">
-                        <Nav.Link
+                      <NavItem className="ml-auto">
+                        <NavLink
                           className="nav-link"
                           to="/logout"
                           onClick={() => context.logoutUser()}
                         >
                           Logout
-                        </Nav.Link>
-                      </Nav.Item>
+                        </NavLink>
+                      </NavItem>
                     </Nav>
                   </Navbar.Collapse>
                 </Navbar>
