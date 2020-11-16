f082b9860530ff9d0a6bccaacbb5aa6b6924fe0a
Hesam Mashhadi Mohammad <s271199@studenti.polito.it>
2020-11-16 21:20:51 +0100

    dev: editing API.js [PULSEBS-35]

--- client/src/components/Student.js
+++ client/src/components/Student.js
@@ -4,34 +4,7 @@ import LectureList from "./Lectures";
 
 class Student extends Component {
   state = {
-    lectures: [
-      {
-        lectureId: 1,
-        courseId: 12,
-        courseName: "Data",
-        startTS: 2020,
-        endTS: 2021,
-        online: 0,
-        teacherName: "Hesam",
-        roomName: 21,
-        bookedSeats: 12,
-        totalSeats: 33,
-        bookedSelf: 1,
-      },
-      {
-        lectureId: 2,
-        courseId: 32,
-        courseName: "SE2",
-        startTS: 2020,
-        endTS: 2021,
-        online: 0,
-        teacherName: "Mehrdad",
-        roomName: 12,
-        bookedSeats: 44,
-        totalSeats: 100,
-        bookedSelf: 0,
-      },
-    ],
+    lectures: [],
     bookingProgres: 0,
   };
 
@@ -51,13 +24,13 @@ class Student extends Component {
   };
 
   componentDidMount() {
-    // //getting list of all lectures
-    // const studentId = 2;
-    // API.getLectures(studentId)
-    //   .then((lectures) => {
-    //     this.setState({ lectures : lectures });
-    //   })
-    //   .catch((err) => console.log(err));
+    //getting list of all lectures
+    const studentId = 1;
+    API.getLectures(studentId)
+      .then((lectures) => {
+        this.setState({ lectures : lectures });
+      })
+      .catch((err) => console.log(err));
   }
 
   render() {
