3efa01e9c622f3287ad2d8b8b3f33b1d12d86dd0
Alessandro Borione <s272070@studenti.polito.it>
2020-11-17 21:03:32 +0100

    Add test cases [PULSEBS-29]

---
+++ server/tests/BookLectureTest.php
@@ -0,0 +1,205 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+use GuzzleHttp\Client;
+
+class BookLectureTest extends TestCase {
+
+	public static function setUpBeforeClass(): void {
+		copy('../db.sqlite', '../db.sqlite.backup');
+	}
+
+	protected function setUp(): void {
+		copy('../testdb.sqlite', '../db.sqlite');
+	}
+
+	protected function tearDown(): void {
+	}
+
+	public static function tearDownAfterClass(): void {
+		copy('../db.sqlite.backup', '../db.sqlite');
+		unlink('../db.sqlite.backup');
+	}
+
+	// Book one seat for online
+	public function test_book_1() {
+		$client = new Client(array('http_errors' => false));
+		$jar = new \GuzzleHttp\Cookie\CookieJar;
+
+		$data = array(
+			'username' => 'francescop',
+			'password' => '123456789',
+		);
+		$response = $client->request('POST', API_PATH . '/api/login', array('form_params' => $data, 'cookies' => $jar));
+		$this->assertEquals(200, $response->getStatusCode());
+
+		$response = $client->request('POST', API_PATH . '/api/users/1/book', array('form_params' => array('lectureId' => 1), 'cookies' => $jar));
+		$this->assertEquals(200, $response->getStatusCode());
+		$data = json_decode($response->getBody(true), true);
+
+		$this->assertArrayHasKey('success', $data);
+		$this->assertTrue($data['success']);
+	}
+
+	// Book two seats for online
+	public function test_book_2() {
+		$client = new Client(array('http_errors' => false));
+		$jar = new \GuzzleHttp\Cookie\CookieJar;
+
+		$data = array(
+			'username' => 'francescop',
+			'password' => '123456789',
+		);
+		$response = $client->request('POST', API_PATH . '/api/login', array('form_params' => $data, 'cookies' => $jar));
+		$this->assertEquals(200, $response->getStatusCode());
+
+		$response = $client->request('POST', API_PATH . '/api/users/1/book', array('form_params' => array('lectureId' => 1), 'cookies' => $jar));
+		$this->assertEquals(200, $response->getStatusCode());
+		$data = json_decode($response->getBody(true), true);
+
+		$this->assertArrayHasKey('success', $data);
+		$this->assertTrue($data['success']);
+
+		$response = $client->request('POST', API_PATH . '/api/users/1/book', array('form_params' => array('lectureId' => 1), 'cookies' => $jar));
+		$this->assertEquals(200, $response->getStatusCode());
+		$data = json_decode($response->getBody(true), true);
+
+		$this->assertArrayHasKey('success', $data);
+		$this->assertFalse($data['success']);
+	}
+
+	// Book one seat for presence
+	public function test_book_3() {
+		$client = new Client(array('http_errors' => false));
+		$jar = new \GuzzleHttp\Cookie\CookieJar;
+
+		$data = array(
+			'username' => 'francescop',
+			'password' => '123456789',
+		);
+		$response = $client->request('POST', API_PATH . '/api/login', array('form_params' => $data, 'cookies' => $jar));
+		$this->assertEquals(200, $response->getStatusCode());
+
+		$response = $client->request('POST', API_PATH . '/api/users/1/book', array('form_params' => array('lectureId' => 2), 'cookies' => $jar));
+		$this->assertEquals(200, $response->getStatusCode());
+		$data = json_decode($response->getBody(true), true);
+
+		$this->assertArrayHasKey('success', $data);
+		$this->assertFalse($data['success']);
+	}
+
+	// Book one seat for cancelled
+	public function test_book_4() {
+		$client = new Client(array('http_errors' => false));
+		$jar = new \GuzzleHttp\Cookie\CookieJar;
+
+		$data = array(
+			'username' => 'francescop',
+			'password' => '123456789',
+		);
+		$response = $client->request('POST', API_PATH . '/api/login', array('form_params' => $data, 'cookies' => $jar));
+		$this->assertEquals(200, $response->getStatusCode());
+
+		$response = $client->request('POST', API_PATH . '/api/users/1/book', array('form_params' => array('lectureId' => 3), 'cookies' => $jar));
+		$this->assertEquals(200, $response->getStatusCode());
+		$data = json_decode($response->getBody(true), true);
+
+		$this->assertArrayHasKey('success', $data);
+		$this->assertFalse($data['success']);
+	}
+
+	// Book but not follow course
+	public function test_book_5() {
+		$client = new Client(array('http_errors' => false));
+		$jar = new \GuzzleHttp\Cookie\CookieJar;
+
+		$data = array(
+			'username' => 'francescop',
+			'password' => '123456789',
+		);
+		$response = $client->request('POST', API_PATH . '/api/login', array('form_params' => $data, 'cookies' => $jar));
+		$this->assertEquals(200, $response->getStatusCode());
+
+		$response = $client->request('POST', API_PATH . '/api/users/1/book', array('form_params' => array('lectureId' => 4), 'cookies' => $jar));
+		$this->assertEquals(200, $response->getStatusCode());
+		$data = json_decode($response->getBody(true), true);
+
+		$this->assertArrayHasKey('success', $data);
+		$this->assertFalse($data['success']);
+	}
+
+	// Book but lecture past
+	public function test_book_6() {
+		$client = new Client(array('http_errors' => false));
+		$jar = new \GuzzleHttp\Cookie\CookieJar;
+
+		$data = array(
+			'username' => 'francescop',
+			'password' => '123456789',
+		);
+		$response = $client->request('POST', API_PATH . '/api/login', array('form_params' => $data, 'cookies' => $jar));
+		$this->assertEquals(200, $response->getStatusCode());
+
+		$response = $client->request('POST', API_PATH . '/api/users/1/book', array('form_params' => array('lectureId' => 5), 'cookies' => $jar));
+		$this->assertEquals(200, $response->getStatusCode());
+		$data = json_decode($response->getBody(true), true);
+
+		$this->assertArrayHasKey('success', $data);
+		$this->assertFalse($data['success']);
+	}
+
+	// Book but room full
+	public function test_book_7() {
+		$client = new Client(array('http_errors' => false));
+		$jar = new \GuzzleHttp\Cookie\CookieJar;
+
+		$data = array(
+			'username' => 'francescop',
+			'password' => '123456789',
+		);
+		$response = $client->request('POST', API_PATH . '/api/login', array('form_params' => $data, 'cookies' => $jar));
+		$this->assertEquals(200, $response->getStatusCode());
+
+		$response = $client->request('POST', API_PATH . '/api/users/1/book', array('form_params' => array('lectureId' => 6), 'cookies' => $jar));
+		$this->assertEquals(200, $response->getStatusCode());
+		$data = json_decode($response->getBody(true), true);
+
+		$this->assertArrayHasKey('success', $data);
+		$this->assertFalse($data['success']);
+	}
+
+	// Book but teacher
+	public function test_book_8() {
+		$client = new Client(array('http_errors' => false));
+		$jar = new \GuzzleHttp\Cookie\CookieJar;
+
+		$data = array(
+			'username' => 'marcot',
+			'password' => 'passw1',
+		);
+		$response = $client->request('POST', API_PATH . '/api/login', array('form_params' => $data, 'cookies' => $jar));
+		$this->assertEquals(200, $response->getStatusCode());
+
+		$response = $client->request('POST', API_PATH . '/api/users/1/book', array('form_params' => array('lectureId' => 1), 'cookies' => $jar));
+		$this->assertEquals(200, $response->getStatusCode());
+		$data = json_decode($response->getBody(true), true);
+
+		$this->assertArrayHasKey('success', $data);
+		$this->assertFalse($data['success']);
+	}
+
+	// Book but unauthenticated
+	public function test_book_9() {
+		$client = new Client(array('http_errors' => false));
+
+		$response = $client->request('POST', API_PATH . '/api/users/1/book', array('form_params' => array('lectureId' => 1)));
+		$this->assertEquals(403, $response->getStatusCode());
+		$data = json_decode($response->getBody(true), true);
+
+		$this->assertArrayHasKey('success', $data);
+		$this->assertFalse($data['success']);
+
+		$this->assertArrayHasKey('reason', $data);
+	}
+}
