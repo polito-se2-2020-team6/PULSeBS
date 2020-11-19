571e2c3ccea3ced1aee43b7de9da859f62d7e912
Mostafa Tavassoli Norouzi <68356134+mosTavassoli@users.noreply.github.com>
2020-11-19 22:01:36 +0100

    feat: improve the Login UI [PULSEBS-45]

--- client/src/components/LoginPage.js
+++ client/src/components/LoginPage.js
@@ -1,14 +1,21 @@
 import React from "react";
 import {
   Form,
-  FormGroup,
+  // FormGroup,
   Col,
-  Button,
-  FormControl,
+  // Button,
+  // FormControl,
   Container,
   Row,
 } from "react-bootstrap";
-// import { ROLES } from "../data/consts";
+import { ROLES } from "../data/consts";
+import {
+  FormControl,
+  Input,
+  InputLabel,
+  FormGroup,
+  Button,
+} from "@material-ui/core";
 import { AuthContext } from "../auth/AuthContext";
 
 class LoginPage extends React.Component {
@@ -46,15 +53,70 @@ class LoginPage extends React.Component {
           <Container fluid className="center center mt-5 ">
             <Row>
               <Col className="center center mt-5 " md={{ span: 4, offset: 4 }}>
-                <h3 className="center mb-3"> Login </h3>
-
                 <Form
                   method="POST"
                   onSubmit={(event) =>
                     this.handleSubmit(event, context.loginUser)
                   }
                 >
-                  {/* <FormGroup>
+                  <FormGroup>
+                    <FormControl>
+                      <InputLabel htmlFor="my-input">Username</InputLabel>
+                      <Input
+                        id="username"
+                        name="username"
+                        placeholder="Username"
+                        value={this.state.username}
+                        onChange={this.handleInputChange}
+                        required
+                        autoFocus
+                        className="mb-3"
+                        // aria-describedby="my-helper-text"
+                      />
+                      {/* <FormHelperText id="my-helper-text">
+                        We'll never share your email.
+                      </FormHelperText> */}
+                    </FormControl>
+
+                    <FormControl>
+                      <InputLabel htmlFor="my-input">Password</InputLabel>
+                      <Input
+                        type="password"
+                        id="password"
+                        name="password"
+                        placeholder="Password"
+                        value={this.state.password}
+                        onChange={this.handleInputChange}
+                        required
+                        
+                      />
+                      {/* <FormHelperText id="my-helper-text">
+                      We'll never share your email.
+                    </FormHelperText> */}
+                    </FormControl>
+                  </FormGroup>
+                  <FormGroup>
+                    <Button
+                      block
+                      type="submit"
+                      color="primary"
+                      // size="sm"
+                      size="small"
+                      variant="contained"
+                      className="mt-5"
+                    >
+                      Login
+                    </Button>
+                  </FormGroup>
+                </Form>
+
+                {/* <Form
+                  method="POST"
+                  onSubmit={(event) =>
+                    this.handleSubmit(event, context.loginUser)
+                  }
+                >
+                  <FormGroup>
                     <select
                       className="form-control"
                       role={this.state.role}
@@ -66,7 +128,7 @@ class LoginPage extends React.Component {
                       <option value="teacher">Booking Manager</option>
                       <option value="student">Support Officer</option>
                     </select>
-                  </FormGroup> */}
+                  </FormGroup>
                   <FormGroup>
                     <FormControl
                       type="text"
@@ -102,7 +164,7 @@ class LoginPage extends React.Component {
                       Login
                     </Button>
                   </FormGroup>
-                </Form>
+                </Form> */}
               </Col>
             </Row>
           </Container>
