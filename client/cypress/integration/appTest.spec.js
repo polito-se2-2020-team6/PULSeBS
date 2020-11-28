8a199ddfd034b089050c3a3b4eeeb867771c1400
Mostafa Tavassoli Norouzi <68356134+mosTavassoli@users.noreply.github.com>
2020-11-28 18:14:33 +0100

    fix: changes in testing code [PULSEBS-79]

--- client/cypress/integration/appTest.spec.js
+++ client/cypress/integration/appTest.spec.js
@@ -1,3 +1,5 @@
+const url = "http://localhost:3000";
+
 context("Home Page", () => {
   it("Login Page Test", () => {
     cy.visit(url);
@@ -7,25 +9,25 @@ context("Home Page", () => {
   });
 });
 
-// context("Student Page", () => {
-//   it("Login Test for Student Component", () => {
-//     cy.visit(url);
-//     cy.get("#username").type("mostafat");
-//     cy.get("#password").type("helloworld");
-//     cy.get("#login").click();
-//     cy.url().should("include", "/student/home");
-//     cy.get("h1").should("have.text", "Book Your Next Lectures");
-//     cy.get("#calendar").click();
-//     cy.url().should("include", "/student/calendar");
-//   });
-// });
+context("Student Page", () => {
+  it("Login Test for Student Component", () => {
+    cy.visit(url);
+    cy.get("#username").type("mostafat");
+    cy.get("#password").type("helloworld");
+    cy.get("#login").click();
+    cy.url().should("include", "/student/home");
+    cy.get("h1").should("have.text", "Book Your Next Lectures");
+    cy.get("#calendar").click();
+    cy.url().should("include", "/student/calendar");
+  });
+});
 
-// context("Teacher Page", () => {
-//   it("Login Test for Teacher Component", () => {
-//     cy.visit(url);
-//     cy.get("#username").type("marcot");
-//     cy.get("#password").type("passw1");
-//     cy.get("#login").click();
-//     cy.url().should("include", "/teacher/home");
-//   });
-// });
+context("Teacher Page", () => {
+  it("Login Test for Teacher Component", () => {
+    cy.visit(url);
+    cy.get("#username").type("marcot");
+    cy.get("#password").type("passw1");
+    cy.get("#login").click();
+    cy.url().should("include", "/teacher/home");
+  });
+});
