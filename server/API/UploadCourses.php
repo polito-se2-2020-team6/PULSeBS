649e30259f5b8edf820d943ed7bfc76a3ac90630
Alessandro Borione <leax_xiv@me.com>
2020-12-11 18:01:26 +0100

    Update UploadCourses [PULSEBS-105]
    Refactor to use built-in csv support.

--- server/API/UploadCourses.php
+++ server/API/UploadCourses.php
@@ -22,21 +22,7 @@ if (!function_exists("upload_courses")) {
 				throw new Exception('Excpected text field.');
 			}
 
-			$lines = explode('\n', $_POST['text']);
-			$titles = explode(',', $lines[0], 5);
-			array_splice($lines, 0, 1);
-
-			$positions = [
-				CODE => array_search(CODE, $titles),
-				YEAR => array_search(YEAR, $titles),
-				SEMESTER => array_search(SEMESTER, $titles),
-				COURSE => array_search(COURSE, $titles),
-				TEACHER => array_search(TEACHER, $titles)
-			];
-
-			if (array_search(false, $positions)) {
-				throw new Exception('Malformed input.');
-			}
+			$lines = str_getcsv($_POST['text'], ',', '"');
 
 			$teachers = get_list_of_teachers();
 
@@ -44,17 +30,16 @@ if (!function_exists("upload_courses")) {
 			$pdo->beginTransaction();
 			$stmt = $pdo->prepare('INSERT INTO courses (code, name, teacher_id, year, semester) VALUES (:code, :name, :teacherId, :year, :semester);');
 			foreach ($lines as $l) {
-				$fields = explode(',', $l, 5);
-				$teacherId = substr($fields[$positions[TEACHER]], 1);
+				$teacherId = substr($l[TEACHER], 1);
 				if (!array_search($teacherId, $teachers)) {
 					$pdo->rollBack();
 					throw new Exception('Malformed input.');
 				}
-				$stmt->bindValue(':code', $fields[$positions[CODE]], PDO::PARAM_STR);
-				$stmt->bindValue(':name', $fields[$positions[COURSE]], PDO::PARAM_STR);
-				$stmt->bindValue(':teacherId', $teacherId, PDO::PARAM_INT);
-				$stmt->bindValue(':year', $fields[$positions[YEAR]], PDO::PARAM_INT);
-				$stmt->bindValue(':semester', $fields[$positions[SEMESTER]], PDO::PARAM_INT);
+				$stmt->bindValue(':code', $l[CODE], PDO::PARAM_STR);
+				$stmt->bindValue(':name', $l[COURSE], PDO::PARAM_STR);
+				$stmt->bindValue(':teacherId', intval($teacherId), PDO::PARAM_INT);
+				$stmt->bindValue(':year', intval($l[YEAR]), PDO::PARAM_INT);
+				$stmt->bindValue(':semester', intval($l[SEMESTER]), PDO::PARAM_INT);
 
 				if (!$stmt->execute()) {
 					$pdo->rollBack();
@@ -63,9 +48,7 @@ if (!function_exists("upload_courses")) {
 			}
 
 			$pdo->commit();
-			echo json_encode([
-				'success' => true
-			]);
+			echo json_encode(['success' => true]);
 		} catch (Exception $e) {
 			echo json_encode([
 				'success' => false,
