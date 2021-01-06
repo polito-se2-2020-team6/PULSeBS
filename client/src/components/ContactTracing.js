044cbd33c7a03618a63e58761afab9b66995d4eb
mosTavassoli <m.tavassoli.it@gmail.com>
2021-01-06 18:06:26 +0100

    feat: update with sample content [PULSEBS-124]

--- client/src/components/ContactTracing.js
+++ client/src/components/ContactTracing.js
@@ -1,8 +1,20 @@
 import React from "react";
-
+import { Container, Jumbotron } from "react-bootstrap";
 class ContactTracing extends React.Component {
   render() {
-    return <p>Hi</p>;
+    return (
+      <>
+        <Jumbotron fluid>
+          <Container>
+            <h1>Contact Tracing Report</h1>
+            <p>
+              A page for reporting the positive students to comply with safety
+              regulations.
+            </p>
+          </Container>
+        </Jumbotron>
+      </>
+    );
   }
 }
 
