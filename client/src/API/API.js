6cf67db69d15f337212099daeb44a9e5348635cf
Hesam Mashhadi Mohammad <s271199@studenti.polito.it>
2021-01-12 20:24:08 +0100

    small change

--- client/src/API/API.js
+++ client/src/API/API.js
@@ -496,30 +496,30 @@ async function getAllCoursesSO() {
 
 //Edit a course Schedule
 async function editSchedule(courseId, data) {
-  // return a new promise.
-  return new Promise(function (resolve, reject) {
-    // do the usual XHR stuff
-    var req = new XMLHttpRequest();
-    let url = baseURL + `courses/${courseId}/schedule`;
-    let data = `lectureId=${lectureId}`;
-    req.open("post", url);
-    //NOW WE TELL THE SERVER WHAT FORMAT OF POST REQUEST WE ARE MAKING
-    req.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    req.onload = function () {
-      if (req.status === 200) {
-        const response = req.response;
-        let obj = JSON.parse(response);
-        resolve(obj);
-      } else {
-        reject(Error(req.statusText));
-      }
-    };
-    // handle network errors
-    req.onerror = function () {
-      reject(Error("Network Error"));
-    }; // make the request
-    req.send(data);
-  });
+  // // return a new promise.
+  // return new Promise(function (resolve, reject) {
+  //   // do the usual XHR stuff
+  //   var req = new XMLHttpRequest();
+  //   let url = baseURL + `courses/${courseId}/schedule`;
+  //   let data = `lectureId=${lectureId}`;
+  //   req.open("post", url);
+  //   //NOW WE TELL THE SERVER WHAT FORMAT OF POST REQUEST WE ARE MAKING
+  //   req.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  //   req.onload = function () {
+  //     if (req.status === 200) {
+  //       const response = req.response;
+  //       let obj = JSON.parse(response);
+  //       resolve(obj);
+  //     } else {
+  //       reject(Error(req.statusText));
+  //     }
+  //   };
+  //   // handle network errors
+  //   req.onerror = function () {
+  //     reject(Error("Network Error"));
+  //   }; // make the request
+  //   req.send(data);
+  // });
 }
 
 
